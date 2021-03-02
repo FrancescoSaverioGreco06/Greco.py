0c5097439c5a5f61334a3eabc2c1c5c46b1a64dc
FrancescoSaverioGreco06 <greco.francescosaverio.s30@liceoviconapoli.it>
2021-03-02 16:11:25 +0100

    Create jsonlibreria.py

---
+++ jsonlibreria/jsonlibreria.py
@@ -0,0 +1,67 @@
+import json
+
+
+Libri_string = '''
+{
+    "libri": [
+        {
+            "titolo": "Capitani coraggiosi",
+            "genere": "Romanzo",
+            "anno pubblicazione": "1897",
+            "autore": " Rudyard Kipling"
+        },
+        {
+            "titolo": "Coach Wooden and me",
+            "genere": "Biografia",
+            "anno pubblicazione": "2017",
+            "autore": "Kareem Abdul-Jabbar"
+        },
+        {
+            "titolo": "Diaro di un senza fissa dimora",
+            "genere": "Scienze sociali",
+            "anno pubblicazione": "2011",
+            "autore": "Marc Augè"
+        },
+        {
+            "titolo": "Lo strano caso del cane ucciso a mezzanotte",
+            "genere": "Narrativa moderna e contemporanea",
+            "anno pubblicazione": "2014",
+            "autore": "Mark Haddon"
+        },
+        {
+            "titolo": "Per questo mi chiamo Giovanni",
+            "genere": "6-9",
+            "anno pubblicazione": "2019",
+            "autore": "Luigi Garlando"
+        },
+        {
+            "titolo": "Grand Hotel Calciomercato",
+            "genere": "Calcio", 
+            "anno pubblicazione": "2020", 
+            "autore": "Gianluca di Marzio", 
+        },
+        {
+            "titolo": "io,IBRA",
+            "genere": "sport",
+            "anno pubblicazione": "2013",
+            "autore": "Zlatan Ibrahimovic"
+        },
+        {
+            "titolo": "Un amico segreto in giardino",
+            "genere": "6-9 anni",
+            "anno pubblicazione": "2012",
+            "autore": "Linda Newbery"
+        },
+        {
+            "titolo": "Le bestie giovani",
+            "genere": "Thriller e gialli",
+            "anno pubblicazione": "2021",
+            "autore": "Davide Longo"
+        },
+        {
+            "titolo": "A regà bongiorno",
+            "genere": "umorismo",
+            "anno pubblicazione": "2016",
+            "autore": "Er Faina",
+        }
+'''
