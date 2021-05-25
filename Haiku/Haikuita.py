1656ebbfb6fc5840e73e7b09477008771de641eb
FrancescoSaverioGreco06 <greco.francescosaverio.s30@liceoviconapoli.it>
2021-05-25 09:49:51 +0200

    Update Haikuita.py

--- Haiku/Haikuita.py
+++ Haiku/Haikuita.py
@@ -3,6 +3,13 @@ from csv import reader
 import random
 import tkinter as tk
 
+window = tk.Tk()
+window.geometry("700x500")
+window.title("Generatore di haiku")
+window.resizable(False, False)
+window.configure(background="light grey")
+
+
 def software():
     # Estrattore casuale
     def estrazione(lista): 
@@ -31,21 +38,30 @@ def software():
     x=estrazione(nuova_lista_1) 
     y=estrazione(nuova_lista_2)
     z=estrazione(nuova_lista_3)
-    lista = [x , y , z]
-    text_output = tk.Label(window, text=lista)
-    text_output.grid(row=2, column=0)
-  
+    text_output = tk.Label(window, text=x, fg="dark blue", font=("Times New Roman",20 ))
+    text_output.grid(row=1, column=1)
+    text_output_2 = tk.Label(window, text=y, fg="dark blue", font=("Times New Roman",20 ))
+    text_output_2.grid(row=2, column=1)
+    text_output_3 = tk.Label(window, text=z, fg="dark blue", font=("Times New Roman",20 ))
+    text_output_3.grid(row=3, column=1)
+    text_1=tk.Label(window, text="1° VERSO", fg="black", font=("Times New Roman",18 ))
+    text_1.grid(row=1, column=0)
+    text_2=tk.Label(window, text="2° VERSO", fg="black", font=("Times New Roman",18 ))
+    text_2.grid(row=2, column=0)
+    text_3=tk.Label(window, text="3° VERSO", fg="black", font=("Times New Roman",18 ))
+    text_3.grid(row=3, column=0)
+    text_4=tk.Label(window, text="INIZIO", fg="black", font=("Times New Roman",18 ))
+    text_4.grid(row=1, column=5)
+    text_5=tk.Label(window, text="AZIONE", fg="black", font=("Times New Roman",18 ))
+    text_5.grid(row=2, column=5)
+    text_6=tk.Label(window, text="CONCLUSIONE", fg="black", font=("Times New Roman",18 ))
+    text_6.grid(row=3, column=5)
 
 
 
-window = tk.Tk()
-window.geometry("700x500")
-window.title("Generatore di haiku")
-window.resizable(False, False)
-window.configure(background="light grey")
-text = "Questo programma serve a generare casualmente Haiku, partendo da un insieme di versi. I versi sono divisi in tre categorie." 
-text_output_2= tk.Label(window, text=text)
-text_output_2.grid(row=0, column=0)
-first_button = tk.Button(text="premi il bottone", width=100, command = software)
-first_button.grid(row=1, column=0)
+
+first_button = tk.Button(text="PREMI IL BOTTONE", command = software, font=20, width=20)
+
+first_button.grid(row=0, column=0)
+
 window.mainloop()
